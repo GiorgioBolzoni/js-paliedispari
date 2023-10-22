a352e5b893a4a1d09516b885224965c24b422d6b
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-22 16:39:20 +0200

    esercizio completato ( problemi  a tornare un numero da valore inserito da utente

---
+++ Pari e dispari/js/script.js
@@ -0,0 +1,98 @@
+/* 
+* Chiedere all’utente di inserire un numero da 1 a 5
+* Il computer genera randomicamente un numero da 1 a 5
+fare somma dei 2 numeri
+
+prendere la somma
+l'utente clicca su btn pari o btn dispari (entrambi i btn generano la somma)
+se l'utente indovina se è pari/dispari vince
+ msg = hai vinto
+    
+    
+altrimenti perde
+ msg = hai perso
+stampo
+ */
+
+//chiamo funzione principale (main)
+
+
+//prendo i diversi elementi (btn, input e div in cui stampare msg)
+let inputEl = document.querySelector('input');
+const divPC = document.getElementById('divComputer');
+const btnPari = document.querySelector('.btn-pari');
+const btnDispari = document.querySelector('.btn-dispari');
+const resultEl = document.querySelector('.alert');
+
+let valoreInserito = parseInt(inputEl.value)
+console.log(valoreInserito)
+const numberPC = getRndInteger(1, 100);
+let sumNum = valoreInserito + numberPC;
+
+// console.log(inputEl);
+// console.log(btn);
+// console.log(resultEl);
+
+
+//aggiungo eventListener al bottone
+btnPari.addEventListener('click', function () {
+    // console.log(inputEl)
+    if (isNaN(valoreInserito)) {
+        alert('Devi inserire un numero!');
+    } else {
+        if (sumNum % 2 === 0) {
+            resultEl.innerHTML = `
+            La somma è ${sumNum}
+            <br>
+            Hai vinto!
+                `
+            resultEl.classList.remove(d - none)
+
+        } else {
+            divPC.innerHTML = numberPC;
+
+            resultEl.innerHTML = `
+            La somma è ${sumNum}
+            <br>
+            Hai perso!
+                `
+            resultEl.classList.remove(d - none)
+
+        }
+    }
+
+    // console.log(sumNum)
+
+
+});
+btnDispari.addEventListener('click', function () {
+    if (isNaN(valoreInserito)) {
+        alert('Devi inserire un numero!');
+    } else {
+        if (!sumNum % 2 === 0) {
+            resultEl.innerHTML = `
+            La somma è ${sumNum}
+            <br>
+            Hai vinto!
+                `;
+            resultEl.classList.remove(d - none)
+        } else {
+            divPC.innerHTML = numberPC;
+            resultEl.innerHTML = `
+            La somma è ${sumNum}
+            <br>
+            Hai perso!
+                `
+            resultEl.classList.remove(d - none)
+
+        }
+    }
+
+    // console.log(sumNum)
+
+
+});
+
+
+
+
